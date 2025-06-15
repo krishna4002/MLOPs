e81da5a0ede28ae65e0190b5542835e8712b351a
krishna4002 <krishnagopaljay@gmail.com>
2025-06-15 17:43:56 +0530

    Adding Assignment_2 work

---
+++ Assignment_2/consumer2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from confluent_kafka import Consumer
+
+if __name__ == '__main__':
+
+    config = {
+        'bootstrap.servers': 'localhost:9092',
+        'group.id': 'kafka-python-getting-started',
+        'auto.offset.reset': 'earliest'
+    }
+
+    # Create Consumer instance
+    consumer = Consumer(config)
+
+    # Subscribe to both topics
+    topics = ["dummy", "channel"]
+    consumer.subscribe(topics)
+
+    # Poll for new messages from Kafka and print them.
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                # Initial message consumption may take up to
+                # session.timeout.ms for the consumer group to
+                # rebalance and start consuming
+                print("Waiting...")
+            elif msg.error():
+                print("ERROR: %s".format(msg.error()))
+            else:
+                # Extract the (optional) key and value, and print.
+                print("Consumed event from topic {topic}: key = {key:12} value = {value:12}".format(
+                    topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        consumer.close()
---
+++ Assignment_2/publisher2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from random import choice
+from confluent_kafka import Producer
+
+if __name__ == '__main__':
+
+    config = {
+        # User-specific properties that you must set
+        'bootstrap.servers': 'localhost:9092',
+
+        # Fixed properties
+        'acks': 'all'
+    }
+
+    # Create Producer instance
+    producer1=Producer(config)
+    producer2 = Producer(config)
+
+    # Optional per-message delivery callback (triggered by poll() or flush())
+    # when a message has been successfully delivered or permanently
+    # failed delivery (after retries).
+    def delivery_callback(err, msg):
+        if err:
+            print('ERROR: Message failed delivery: {}'.format(err))
+        else:
+            print("Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
+                topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+
+    # Produce data by selecting random values from these lists.
+    topic =[ "dummy","channel"]
+    user_ids = '42'
+    products = 'Pranab'
+    producer1.produce(topic[0], products, user_ids, callback=delivery_callback)
+    user_ids_2 = '60'
+    products_2 = 'Debraj'
+    producer2.produce(topic[1], products_2, user_ids_2, callback=delivery_callback)
+
+    # Block until the messages are sent.
+    producer1.poll(10000)
+    producer1.flush()
+    producer2.poll(10000)
+    producer2.flush()
