f3266f944bfbec00fd0bc1a12927f36fe3e30477
krishna4002 <krishnagopaljay@gmail.com>
2025-06-18 08:27:48 +0530

    Update view_csv.py

--- Assignment_3/view_csv.py
+++ Assignment_3/view_csv.py
@@ -1,8 +1,8 @@
 # view_csv.py
 import pandas as pd
 
-cls_df = pd.read_csv(r'E:\MLOPs\Assignment_2\database\classification_predictions.csv')
+cls_df = pd.read_csv(r'E:\MLOPs internship\MLOPs\Assignment_3\database\classification_predictions.csv')
 print("\nClassification Predictions:\n", cls_df)
 
-reg_df = pd.read_csv(r'E:\MLOPs\Assignment_2\database\regression_predictions.csv')
+reg_df = pd.read_csv(r'E:\MLOPs internship\MLOPs\Assignment_3\database\regression_predictions.csv')
 print("\nRegression Predictions:\n", reg_df)
